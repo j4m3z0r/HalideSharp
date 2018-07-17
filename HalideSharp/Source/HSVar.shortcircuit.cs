3e28ff1f91a51d2ef996150ca62cdb7dab1d55f0
James Gregory <james@james.id.au>
2018-07-16 20:32:05 -0700

    Move HSVar & HSExpr to LeMP, move their operator true & false overloading to a partial class since LeMP doesn't like that.

---
+++ HalideSharp/Source/HSVar.shortcircuit.cs
@@ -0,0 +1,20 @@
+﻿namespace HalideSharp
+{
+    public partial class HSVar
+    {
+        // HACK: returning false from both operator true and operator false should force us to never short-circuit,
+        // meaning that && will always invoke operator&, which calls C++'s operator&& in the wrapper. In short, this
+        // allows for the common (x && y) style conditionals.
+        public static bool operator true(HSVar v)
+        {
+            return false;
+        }
+
+        public static bool operator false(HSVar v)
+        {
+            return false;
+        }
+
+        
+    }
+}
