20626b71e4cf84325be3f7b93a7419d65cef83a5
James Gregory <james@james.id.au>
2018-07-19 20:10:54 -0700

    Remove old operator overload C implementations, update Expr to new C function names.

--- HalideSharp/Source/hsexpr.cpp
+++ HalideSharp/Source/hsexpr.cpp
@@ -3,35 +3,15 @@
 
 using namespace Halide;
 
-extern "C" Expr* new_expr_int(int i) {
+extern "C" Expr* Expr_New_Int(int i) {
     return new Expr(i);
 }
 
-extern "C" void delete_expr(Expr *e) {
+extern "C" void Expr_Delete(Expr *e) {
     delete e;
 }
 
-// expr <op> float
-extern "C" Expr* expr_mult_float(Expr* e, float f) { return new Expr((*e) * f); }
-
-// expr <op> int
-extern "C" Expr* expr_plus_int(Expr* e, int i) { return new Expr((*e) + i); }
-extern "C" Expr* expr_div_int(Expr* e, int i) { return new Expr((*e) / i); }
-extern "C" Expr* expr_lt_int(Expr* e, int i) { return new Expr((*e) < i); }
-extern "C" Expr* expr_gt_int(Expr* e, int i) { return new Expr((*e) > i); }
-
-// expr <op> expr
-extern "C" Expr* expr_plus_expr(Expr* e1, Expr* e2) { return new Expr((*e1) + (*e2)); }
-extern "C" Expr* expr_minus_expr(Expr* e1, Expr* e2) { return new Expr((*e1) - (*e2)); }
-extern "C" Expr* expr_and_expr(Expr* e1, Expr* e2) { return new Expr((*e1) && (*e2)); }
-
-// expr <op> var
-extern "C" Expr* expr_plus_var(Expr* e, Var *v) { return new Expr((*e) + (*v)); }
-
-// int <op> expr
-extern "C" Expr* int_mult_expr(int i, Expr *e) { return new Expr(i * (*e)); }
-
-extern "C" char* expr_to_string(Expr *e) {
+extern "C" char* Expr_ToString(Expr *e) {
     std::stringstream ss;
     ss << (*e);
     std::string s = ss.str();
