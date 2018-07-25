c96014568c449e49f8e9495cc731252428f831ff
James Gregory <james@james.id.au>
2018-07-24 21:50:24 -0700

    Abuse indexer macros to generate math functions also.

--- HalideSharp/Source/hsmath.cpp
+++ HalideSharp/Source/hsmath.cpp
@@ -4,54 +4,52 @@
 
 using namespace Halide;
 
-#define MATHFN_ONE_ARG(CSNAME, CPPNAME, ARGTYPE) \
-    extern "C" Expr* Global_ ## CSNAME ## _ ## ARGTYPE(ARGTYPE *a) { return new Expr(CPPNAME(*a)); }
+#define MATHFN_ONE_ARG(CSNAME, CPPNAME, T) \
+    extern "C" Expr* Global_ ## CSNAME ## _ ## T(argtype(T) a) { \
+        return new Expr(CPPNAME(deref(T, a))); \
+    }
 
-#define ALL_MATHFN_ONE_ARG(ARGTYPE) \
-    MATHFN_ONE_ARG(Sin, sin, ARGTYPE) \
-    MATHFN_ONE_ARG(Asin, asin, ARGTYPE) \
-    MATHFN_ONE_ARG(Cos, cos, ARGTYPE) \
-    MATHFN_ONE_ARG(Acos, acos, ARGTYPE) \
-    MATHFN_ONE_ARG(Tan, tan, ARGTYPE) \
-    MATHFN_ONE_ARG(Atan, atan, ARGTYPE) \
-    MATHFN_ONE_ARG(Sinh, sinh, ARGTYPE) \
-    MATHFN_ONE_ARG(Asinh, asinh, ARGTYPE) \
-    MATHFN_ONE_ARG(Cosh, cosh, ARGTYPE) \
-    MATHFN_ONE_ARG(Acosh, acosh, ARGTYPE) \
-    MATHFN_ONE_ARG(Tanh, tanh, ARGTYPE) \
-    MATHFN_ONE_ARG(Atanh, atanh, ARGTYPE) \
-    MATHFN_ONE_ARG(Sqrt, sqrt, ARGTYPE) \
-    MATHFN_ONE_ARG(Exp, exp, ARGTYPE) \
-    MATHFN_ONE_ARG(Log, log, ARGTYPE) \
-    MATHFN_ONE_ARG(Erf, erf, ARGTYPE) \
-    MATHFN_ONE_ARG(FastInverse, fast_inverse, ARGTYPE) \
-    MATHFN_ONE_ARG(FastInverseSqrt, fast_inverse_sqrt, ARGTYPE) \
-    MATHFN_ONE_ARG(Floor, floor, ARGTYPE) \
-    MATHFN_ONE_ARG(Ceil, ceil, ARGTYPE) \
-    MATHFN_ONE_ARG(Round, round, ARGTYPE) \
-    MATHFN_ONE_ARG(Trunc, trunc, ARGTYPE) \
-    MATHFN_ONE_ARG(IsNan, is_nan, ARGTYPE) \
-    MATHFN_ONE_ARG(Fract, fract, ARGTYPE)
+#define ALL_MATHFN_ONE_ARG(T) \
+    MATHFN_ONE_ARG(Sin, sin, T) \
+    MATHFN_ONE_ARG(Asin, asin, T) \
+    MATHFN_ONE_ARG(Cos, cos, T) \
+    MATHFN_ONE_ARG(Acos, acos, T) \
+    MATHFN_ONE_ARG(Tan, tan, T) \
+    MATHFN_ONE_ARG(Atan, atan, T) \
+    MATHFN_ONE_ARG(Sinh, sinh, T) \
+    MATHFN_ONE_ARG(Asinh, asinh, T) \
+    MATHFN_ONE_ARG(Cosh, cosh, T) \
+    MATHFN_ONE_ARG(Acosh, acosh, T) \
+    MATHFN_ONE_ARG(Tanh, tanh, T) \
+    MATHFN_ONE_ARG(Atanh, atanh, T) \
+    MATHFN_ONE_ARG(Sqrt, sqrt, T) \
+    MATHFN_ONE_ARG(Exp, exp, T) \
+    MATHFN_ONE_ARG(Log, log, T) \
+    MATHFN_ONE_ARG(Erf, erf, T) \
+    MATHFN_ONE_ARG(FastInverse, fast_inverse, T) \
+    MATHFN_ONE_ARG(FastInverseSqrt, fast_inverse_sqrt, T) \
+    MATHFN_ONE_ARG(Floor, floor, T) \
+    MATHFN_ONE_ARG(Ceil, ceil, T) \
+    MATHFN_ONE_ARG(Round, round, T) \
+    MATHFN_ONE_ARG(Trunc, trunc, T) \
+    MATHFN_ONE_ARG(IsNan, is_nan, T) \
+    MATHFN_ONE_ARG(Fract, fract, T)
 
 PERMUTE_ARGS_1D(ALL_MATHFN_ONE_ARG)
 
-#define MATHFN_TWO_ARG(CSNAME, CPPNAME, CSTYPE1, CPPTYPE1, DEREF1, CSTYPE2, CPPTYPE2, DEREF2) \
-    extern "C" Expr* Global_ ## CSNAME ## _ ## CSTYPE1 ## CSTYPE2(CPPTYPE1 a, CPPTYPE2 b) { \
-        return new Expr(CPPNAME(DEREF1(a), DEREF2(b))); \
+#define MATHFN_TWO_ARG(CSNAME, CPPNAME, CSTYPE1, CSTYPE2) \
+    extern "C" Expr* Global_ ## CSNAME ## _ ## CSTYPE1 ## CSTYPE2( \
+        argtype(CSTYPE1) a, \
+        argtype(CSTYPE2) b \
+    ) { \
+        return new Expr(CPPNAME(deref(CSTYPE1, a), deref(CSTYPE2, b))); \
     }
 
-#define ALL_MATHFN_TWO_ARG(CSTYPE1, CPPTYPE1, DEREF1, CSTYPE2, CPPTYPE2, DEREF2) \
-    MATHFN_TWO_ARG(Atan2, atan2, CSTYPE1, CPPTYPE1, DEREF1, CSTYPE2, CPPTYPE2, DEREF2) \
-    MATHFN_TWO_ARG(Hypot, hypot, CSTYPE1, CPPTYPE1, DEREF1, CSTYPE2, CPPTYPE2, DEREF2) \
-    MATHFN_TWO_ARG(Pow, pow, CSTYPE1, CPPTYPE1, DEREF1, CSTYPE2, CPPTYPE2, DEREF2) \
-    MATHFN_TWO_ARG(FastPow, fast_pow, CSTYPE1, CPPTYPE1, DEREF1, CSTYPE2, CPPTYPE2, DEREF2)
+#define ALL_MATHFN_TWO_ARG(CSTYPE1, CSTYPE2) \
+    MATHFN_TWO_ARG(Atan2, atan2, CSTYPE1, CSTYPE2) \
+    MATHFN_TWO_ARG(Hypot, hypot, CSTYPE1, CSTYPE2) \
+    MATHFN_TWO_ARG(Pow, pow, CSTYPE1, CSTYPE2) \
+    MATHFN_TWO_ARG(FastPow, fast_pow, CSTYPE1, CSTYPE2)
 
-#define ALL_MATHFN_TWO_ARG_TYPE2(CSTYPE1, CPPTYPE1, DEREF1) \
-    ALL_MATHFN_TWO_ARG(CSTYPE1, CPPTYPE1, DEREF1, Var, Var*, DEREF_POINTER) \
-    ALL_MATHFN_TWO_ARG(CSTYPE1, CPPTYPE1, DEREF1, Expr, Expr*, DEREF_POINTER) \
-    ALL_MATHFN_TWO_ARG(CSTYPE1, CPPTYPE1, DEREF1, Float, float, DEREF_NOPOINTER)
-
-ALL_MATHFN_TWO_ARG_TYPE2(Var, Var*, DEREF_POINTER)
-ALL_MATHFN_TWO_ARG_TYPE2(Expr, Expr*, DEREF_POINTER)
-ALL_MATHFN_TWO_ARG_TYPE2(Float, float, DEREF_NOPOINTER)
+PERMUTE_ARGS_2D(ALL_MATHFN_TWO_ARG)
 
